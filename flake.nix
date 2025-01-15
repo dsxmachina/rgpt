ffe335a4f5dcfe0ea5d7a7e43e7f82d4ccec691c
Christian Hammermeister <christian.hammermeister@borderless-technologies.com>
2025-01-15 13:46:18 +0100

    update flakes

--- flake.nix
+++ flake.nix
@@ -2,24 +2,58 @@
   description = "rgpt: A terminal client for ChatGPT";
 
   inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.follows = "rust-overlay/flake-utils";
-    nixpkgs.follows = "rust-overlay/nixpkgs";
+    flake-utils.url  = "github:numtide/flake-utils";
+    # add compatibility layer to generate shell.nix based on flakes
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
   
   outputs = inputs: with inputs;
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        code = pkgs.callPackage ./. { inherit pkgs system rust-overlay; };
-      in rec {
-        packages = {
-          rgpt = code.rgpt;
-          all = pkgs.symlinkJoin {
-            name = "all";
-            paths = with code; [ rgpt ];
+        inherit (pkgs) lib;
+        # Will be reused in the dev-shell section
+        commonArgs = {
+          nativeBuildInputs = with pkgs; [];
+          buildInputs = with pkgs; [] ++ lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+            pkgs.libiconv
+          ];
+        };
+        # This is necessary, since we import these libraries directly via git in cargo
+
+        # Define the rfm package
+        rfm = pkgs.rustPlatform.buildRustPackage {
+          pname = "rgpt";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock = { 
+            lockFile = ./Cargo.lock; 
           };
-          default = packages.all;
+
+          # use the above build-intputs
+          inherit (commonArgs) nativeBuildInputs buildInputs;
+        };
+      in rec {
+        packages.default = rfm;
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rustc
+            cargo-edit
+            cargo-audit
+            cargo-machete
+            cargo-bloat
+            clippy
+            rustfmt
+          ] ++ commonArgs.nativeBuildInputs ++ commonArgs.buildInputs;
+
+          # Certain Rust tools won't work without this
+          # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+          # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         };
       }
     );
--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
-{ pkgs ? import <nixpkgs> {}}:
-with pkgs;
-mkShell {
-  buildInputs = [ 
-    openssl 
-    pkg-config
-  ];
-  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkg-config";
-}
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).shellNix
